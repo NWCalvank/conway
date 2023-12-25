a2e0686c4c3d1ff081ce29b37cc7cc44b5d5ccf4
Nathan Calvank <nwcalvank@gmail.com>
2023-12-25 14:44:44 -0800

    Install and Run Prettier (#4)
    * Install and Run Prettier
    
    I <3 Prettier, and I don't have strong enough formatting opinions to
    bother with any configuration.
    
    * Remove files list from `jsconfig.json`
    
    It's not needed.
    
    * Retain grid formatting in tests

--- logic.js
+++ logic.js
@@ -5,60 +5,60 @@
  * @returns {boolean} Whether the cell is alive in the next generation.
  */
 function nextGeneration(grid, pos) {
-    const alive = grid[pos];
-    const width = Math.sqrt(grid.length);
-    const row = Math.floor(pos / width);
-    const x = pos - width * row;
+  const alive = grid[pos];
+  const width = Math.sqrt(grid.length);
+  const row = Math.floor(pos / width);
+  const x = pos - width * row;
 
-    const topLeft = width * (row - 1) + (x - 1)
-    const top = width * (row - 1) + x
-    const topRight = width * (row - 1) + (x + 1);
-    const left = width * row + (x - 1)
-    const right = width * row + (x + 1)
-    const bottomLeft = width * (row + 1) + (x - 1)
-    const bottom = width * (row + 1) + x
-    const bottomRight = width * (row + 1) + (x + 1)
+  const topLeft = width * (row - 1) + (x - 1);
+  const top = width * (row - 1) + x;
+  const topRight = width * (row - 1) + (x + 1);
+  const left = width * row + (x - 1);
+  const right = width * row + (x + 1);
+  const bottomLeft = width * (row + 1) + (x - 1);
+  const bottom = width * (row + 1) + x;
+  const bottomRight = width * (row + 1) + (x + 1);
 
-    let neighbours = 0;
+  let neighbours = 0;
 
-    if (row >= 1) {
-        neighbours += grid[top];
-        if (x > 0) {
-            neighbours += grid[topLeft];
-        }
-        if (x < width - 1) {
-            neighbours += grid[topRight];
-        }
+  if (row >= 1) {
+    neighbours += grid[top];
+    if (x > 0) {
+      neighbours += grid[topLeft];
     }
+    if (x < width - 1) {
+      neighbours += grid[topRight];
+    }
+  }
+
+  if (x > 0) {
+    neighbours += grid[left];
+  }
+
+  if (x < width - 1) {
+    neighbours += grid[right];
+  }
+
+  if (pos < grid.length - width) {
+    neighbours += grid[bottom];
 
     if (x > 0) {
-        neighbours += grid[left];
+      neighbours += grid[bottomLeft];
     }
-    
+
     if (x < width - 1) {
-        neighbours += grid[right];
+      neighbours += grid[bottomRight];
     }
+  }
 
-    if (pos < grid.length - width) {
-        neighbours += grid[bottom];
+  if (!alive) {
+    // Birth: Dead -> Live if 3 neighbours
+    return neighbours === 3;
+  }
 
-        if (x > 0) {
-            neighbours += grid[bottomLeft];
-        }
-
-        if (x < width - 1) {
-            neighbours += grid[bottomRight];
-        }
-    }
-    
-    if (!alive) {
-        // Birth: Dead -> Live if 3 neighbours
-        return neighbours === 3;
-    }
-
-    // Survive: Live -> Live if 2-3 neighbours
-    // Death: Live -> Dead if < 3 neighbours
-    return [2, 3].includes(neighbours);
+  // Survive: Live -> Live if 2-3 neighbours
+  // Death: Live -> Dead if < 3 neighbours
+  return [2, 3].includes(neighbours);
 }
 
 /**
@@ -67,10 +67,10 @@ function nextGeneration(grid, pos) {
  * @returns {Array<number>} A new grid representing the next generation.
  */
 export function tick(prev) {
-    const grid = Array(prev.length).fill(0);
-    for (let i = 0; i < prev.length; i++) {
-        grid[i] = +nextGeneration(prev, i);
-    }
+  const grid = Array(prev.length).fill(0);
+  for (let i = 0; i < prev.length; i++) {
+    grid[i] = +nextGeneration(prev, i);
+  }
 
-    return grid;
+  return grid;
 }
--- test.js
+++ test.js
@@ -1,55 +1,57 @@
-import { tick } from './logic.js';
+import { tick } from "./logic.js";
 
 test();
 
 function test() {
-    console.info("Running tests...");
+  console.info("Running tests...");
 
-    const tests = [
-        [[0], [0]],
-        [[1], [0]],
-        [[0,0
-         ,0,0],
-         [0,0
-         ,0,0]],
-        [[1,1
-         ,0,1],
-         [1,1
-         ,1,1]],
-        [[0,0,0
-         ,0,1,0
-         ,0,0,0],
-         [0,0,0
-         ,0,0,0
-         ,0,0,0]],
-        [[0,1,0
-         ,0,1,1
-         ,0,0,0],
-         [0,1,1
-         ,0,1,1
-         ,0,0,0]],
-        [[0,1,0
-         ,1,1,1
-         ,1,0,0],
-         [1,1,1
-         ,1,0,1
-         ,1,0,0]],
-        [[0,1,0,0
-         ,0,0,0,1
-         ,1,1,1,1
-         ,1,0,1,0],
-         [0,0,0,0
-         ,1,0,0,1
-         ,1,0,0,1
-         ,1,0,1,1]],
-    ];
-    for (let [input, expected] of tests) {
-        const res = tick(input);
-        const passed = compare(res, expected);
-        if (passed) {
-            console.info("PASS");
-        }
+  // prettier-ignore
+  const tests = [
+    [[0], [0]],
+    [[1], [0]],
+    [[0,0
+     ,0,0],
+     [0,0
+     ,0,0]],
+    [[1,1
+     ,0,1],
+     [1,1
+     ,1,1]],
+    [[0,0,0
+     ,0,1,0
+     ,0,0,0],
+     [0,0,0
+     ,0,0,0
+     ,0,0,0]],
+    [[0,1,0
+     ,0,1,1
+     ,0,0,0],
+     [0,1,1
+     ,0,1,1
+     ,0,0,0]],
+    [[0,1,0
+     ,1,1,1
+     ,1,0,0],
+     [1,1,1
+     ,1,0,1
+     ,1,0,0]],
+    [[0,1,0,0
+     ,0,0,0,1
+     ,1,1,1,1
+     ,1,0,1,0],
+     [0,0,0,0
+     ,1,0,0,1
+     ,1,0,0,1
+     ,1,0,1,1]],
+  ];
+
+  for (let [input, expected] of tests) {
+    const res = tick(input);
+    const passed = compare(res, expected);
+    if (passed) {
+      console.info("PASS");
     }
+  }
 }
 
 /**
@@ -58,26 +60,21 @@ function test() {
  * @param {Array<number>} grid2 - The expected grid.
  */
 function compare(grid1, grid2) {
-    const sameLength = grid1.length === grid2.length;
-    if (!sameLength) {
-        console.assert(
-            sameLength,
-            "Lengths don't match: %o",
-            {got: grid1.length, exp: grid2.length}
-        );
-        return false;
-    }
+  const sameLength = grid1.length === grid2.length;
+  if (!sameLength) {
+    console.assert(sameLength, "Lengths don't match: %o", {
+      got: grid1.length,
+      exp: grid2.length,
+    });
+    return false;
+  }
 
-    for (let i = 0; i < grid2.length; i++) {
-        const same = grid1[i] === grid2[i]
-        if (!same) {
-            console.assert(
-                same,
-                "Grids don't match: %o",
-                {got: grid1, exp: grid2}
-            );
-            return false;
-        }
+  for (let i = 0; i < grid2.length; i++) {
+    const same = grid1[i] === grid2[i];
+    if (!same) {
+      console.assert(same, "Grids don't match: %o", { got: grid1, exp: grid2 });
+      return false;
     }
-    return true;
+  }
+  return true;
 }
